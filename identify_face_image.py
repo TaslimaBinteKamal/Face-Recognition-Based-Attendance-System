b7b1a5bd9882356c9153189f4a3e771b92659739
TaslimaBinteKamal <44751387+TaslimaBinteKamal@users.noreply.github.com>
2019-11-14 23:44:30 +0600

    Update identify_face_image.py

--- identify_face_image.py
+++ identify_face_image.py
@@ -1,4 +1,3 @@
-#recognition of faces from images
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -13,18 +12,20 @@ import time
 import pickle
 import sys
 from PIL import Image, ImageEnhance 
-img_path='input_13.jpg'
+img_path='9.JPG'
 modeldir = './model/20170511-185253.pb'
 classifier_filename = './class/classifier.pkl'
 npy='./npy'
 train_img="./train_img"
-test_image_processsing = "/home/koli/Downloads/Facenet/test_imge_cropped/"
+test_image_processsing = "/home/koli/Desktop/SPL-3-v1/flask codes/test_imge_cropped/"
+#Create the identity filter, but with the 1 shifted to the right!
 kernel = np.zeros( (9,9), np.float32)
 kernel[4,4] = 2.0   #Identity, times two! 
 
-
+#Create a box filter:
 boxFilter = np.ones( (9,9), np.float32) / 81.0
 
+#Subtract the two:
 kernel = kernel - boxFilter
 
 with tf.Graph().as_default():
@@ -33,9 +34,9 @@ with tf.Graph().as_default():
     with sess.as_default():
         pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
 
-        minsize = 20  
-        threshold = [0.6, 0.7, 0.7]  
-        factor = 0.709  
+        minsize = 20  # minimum size of face
+        threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+        factor = 0.709  # scale factor
         margin = 44
         frame_interval = 3
         batch_size = 1000
@@ -60,7 +61,7 @@ with tf.Graph().as_default():
         with open(classifier_filename_exp, 'rb') as infile:
             (model, class_names) = pickle.load(infile)
 
-        
+        # video_capture = cv2.VideoCapture("akshay_mov.mp4")
         c = 0
 
 
@@ -81,10 +82,10 @@ with tf.Graph().as_default():
                 frame = facenet.to_rgb(frame)
             frame = frame[:, :, 0:3]
             bounding_boxes, _ = detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
-            print("bounding_boxes:", bounding_boxes)
+            #print("bounding_boxes:", bounding_boxes)
 
             nrof_faces = bounding_boxes.shape[0]
-            print('Face Detected: %d' % nrof_faces)
+            #print('Face Detected: %d' % nrof_faces)
 
             if nrof_faces > 0:
                 det = bounding_boxes[:, 0:4]
@@ -103,14 +104,17 @@ with tf.Graph().as_default():
                     bb[i][2] = det[i][2]
                     bb[i][3] = det[i][3]
 
-                 
+                    # inner exception
                     if bb[i][0] <= 0 or bb[i][1] <= 0 or bb[i][2] >= len(frame[0]) or bb[i][3] >= len(frame):
                         print('face is too close')
                         continue
 
                     cropped.append(frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :])
                     cropped[i] = facenet.flip(cropped[i], False)
-                   
+                    #image resize
+
+                    #img = cropped[i]
+                    #imResize = cv2.resize(img, (182,182))
 
                     #custom = cv2.filter2D(imResize, -1, kernel)
                     #cv2.imwrite(os.path.join(test_image_processsing , "file"+str(i)+".jpg"), custom)
@@ -130,11 +134,11 @@ with tf.Graph().as_default():
                     feed_dict = {images_placeholder: scaled_reshape[i], phase_train_placeholder: False}
                     emb_array[0, :] = sess.run(embeddings, feed_dict=feed_dict)
                     predictions = model.predict_proba(emb_array)
-                    print("predictions:", predictions)
+                    #print("predictions:", predictions)
                     best_class_indices = np.argmax(predictions, axis=1)
-                    print("best_class_indices:", best_class_indices)
+                    #print("best_class_indices:", best_class_indices)
                     best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
-                    print("Best Class Probabilities:", best_class_probabilities)
+                    #print("Best Class Probabilities:", best_class_probabilities)
                     cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)    #boxing face
                    
                     
@@ -148,8 +152,10 @@ with tf.Graph().as_default():
                         unknown_names =  "Unknown"
                         if HumanNames[best_class_indices[0]] == H_i:
                             result_names = HumanNames[best_class_indices[0]]
+                            print(result_names)
+                            print(len(HumanNames))
                             cv2.putText(frame, result_names, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                      0.5, (0, 0, 255), thickness=1, lineType=2)
+                                      0.6, (0, 0, 255), thickness=1, lineType=2)
                         """
                         else:
                         	cv2.putText(frame, unknown_names, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
@@ -158,8 +164,8 @@ with tf.Graph().as_default():
                         
             else:
                 print('Unable to align')
-        #cv2.imshow('Image.jpg', frame)
-        cv2.imwrite('Output_13.jpg', frame)
+        #cv2.imshow('Image_16.jpg', frame)
+        cv2.imwrite('output_9.png', frame)
         """
         if cv2.waitKey(1000000) & 0xFF == ord('q'):
             sys.exit("Thanks")
