d28df2a6901a9f29189e2a5348a2f1b55e0d378f
TaslimaBinteKamal <44751387+TaslimaBinteKamal@users.noreply.github.com>
2019-12-11 23:05:55 +0600

    Update train_main.py

--- train_main.py
+++ train_main.py
@@ -7,7 +7,7 @@ from classifier import training
 datadir = './pre_img'
 modeldir = './model/20170511-185253.pb'
 classifier_filename = './class/classifier.pkl'
-print ("<===========Start Training================>")
+print ("Start Training")
 obj=training(datadir,modeldir,classifier_filename)
 get_file=obj.main_train()
 sys.exit("All Done")
